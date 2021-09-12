992afe822b19a3688487132c2ea3cee8be0c298d
Alex Friedman <signetcoding@gmail.com>
2021-09-12 21:56:57 +0800

    Formatted db.js

--- server/db/db.js
+++ server/db/db.js
@@ -1,16 +1,18 @@
-const Sequelize = require("sequelize");
+const Sequelize = require('sequelize');
 
-const db = new Sequelize(`postgres://${process.env.NAME}:${process.env.PASSWORD}@${process.env.HOST}:5432/${process.env.DBNAME}`)
+const db = new Sequelize(
+	`postgres://${process.env.NAME}:${process.env.PASSWORD}@${process.env.HOST}:5432/${process.env.DBNAME}`
+);
 
 // Test connection
 const test = async () => {
-  try {
-    await db.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
+	try {
+		await db.authenticate();
+		console.log('Connection has been established successfully.');
+	} catch (error) {
+		console.error('Unable to connect to the database:', error);
+	}
+};
 
 test();
 
